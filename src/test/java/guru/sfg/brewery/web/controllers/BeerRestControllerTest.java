b79d061e2d58d62572966cc26d7168ec8ab7538f
Zakhar Veremchuk <zaharverem@gmail.com>
2024-12-12 17:57:38 +0200

    Permit for all few endpoints, wrote test to test it

---
+++ src/test/java/guru/sfg/brewery/web/controllers/BeerRestControllerTest.java
@@ -0,0 +1,29 @@
+package guru.sfg.brewery.web.controllers;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@WebMvcTest
+public class BeerRestControllerTest extends BaseIT {
+
+    @Test
+    void test() throws Exception {
+        mockMvc.perform(get("/api/v1/beer"))
+                .andExpect(status().isOk());
+    }
+
+    @Test
+    void findBeerById() throws Exception{
+        mockMvc.perform(get("/api/v1/beer/97df0c39-90c4-4ae0-b663-453e8e19c311"))
+                .andExpect(status().isOk());
+    }
+
+    @Test
+    void findBeerByUpc() throws Exception {
+        mockMvc.perform(get("/api/v1/beerUpc/8631234200036"))
+                .andExpect(status().isOk());
+    }
+}
