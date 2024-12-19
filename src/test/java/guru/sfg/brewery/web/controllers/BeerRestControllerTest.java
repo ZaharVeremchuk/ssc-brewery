0d0df2b54dde4e34a745e51cf825ee052445b77b
Zakhar Veremchuk <zaharverem@gmail.com>
2024-12-19 22:55:26 +0200

    Spring security configuration
    Add custom filter before usernameandpasswordAuthenticationFilter

--- src/test/java/guru/sfg/brewery/web/controllers/BeerRestControllerTest.java
+++ src/test/java/guru/sfg/brewery/web/controllers/BeerRestControllerTest.java
@@ -13,7 +13,7 @@ public class BeerRestControllerTest extends BaseIT {
     @Test
     void deleteBeer() throws Exception {
         mockMvc.perform(delete("/api/v1/beer/97df0c39-90c4-4ae0-b663-453e8e19c311")
-                .header("Api-Key", "spring").header("Api-Secret", "guru"))
+                .header("Api-Key", "solo").header("Api-Secret", "solo"))
                 .andExpect(status().isOk());
     }
     @Test
